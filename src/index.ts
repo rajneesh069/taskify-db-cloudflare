bcaefeafa6d1f860984d5b3cc8cd4d4f882bba34
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-08-30 20:01:05 +0530

    make the description field in schema optional and remove the password field's character limit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Context, Hono } from "hono";
 import userRouter from "@/routes/userRoutes";
+import todoRouter from "@/routes/todoRoutes";
+
 const app = new Hono();
 
 app.route("/users", userRouter);
+app.route("/todos", todoRouter);
 
+/* Health Check Route */
 app.get("/", (c: Context) => c.text("Hello Hono!"));
 
 export default app;
